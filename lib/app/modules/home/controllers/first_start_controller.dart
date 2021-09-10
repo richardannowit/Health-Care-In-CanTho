d37f22465beffede21244d387f881647d91091ea
Vo Tu Thien <vothien220518@gmail.com>
2021-09-10 21:24:41 +0700

    User story 8 nguoi dung truy cap ung dung lan dau tien

--- lib/app/modules/home/controllers/first_start_controller.dart
+++ lib/app/modules/home/controllers/first_start_controller.dart
@@ -1,8 +1,6 @@
 import 'package:carousel_slider/carousel_controller.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_healthcare/app/modules/home/views/home_view.dart';
 import 'package:get/get.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class FirstStartController extends GetxController {
   final controller = CarouselController();
@@ -22,62 +20,6 @@ class FirstStartController extends GetxController {
     'Get list of best doctor nearby you',
     'Chatting with doctor before you booking'
   ];
-  var buttonStyle = ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-          foregroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  side: BorderSide(color: Colors.blueAccent))))
-      .obs;
-  var textButton = 'SKIP'.obs;
-
-  Widget buildIndicator() {
-    if (activeIndex == urlImages.length - 1) {
-      textButton = 'NEXT'.obs;
-      buttonStyle = ButtonStyle(
-              backgroundColor:
-                  MaterialStateProperty.all<Color>(Colors.blueAccent),
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      side: BorderSide(color: Colors.blueAccent))))
-          .obs;
-    } else {
-      textButton = 'SKIP'.obs;
-      buttonStyle = ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              foregroundColor:
-                  MaterialStateProperty.all<Color>(Colors.blueAccent),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                      side: BorderSide(color: Colors.blueAccent))))
-          .obs;
-    }
-    return AnimatedSmoothIndicator(
-        activeIndex: activeIndex, count: urlImages.length);
-  }
-
-  Widget buildImage(
-          String urlImage, String textBelowImage, String note, int index) =>
-      Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fitWidth,
-                image: AssetImage(urlImage),
-              ),
-            ),
-          ),
-        ),
-        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(textBelowImage, style: TextStyle(fontSize: 24)),
-          Text(note)
-        ])
-      ]);
 
   next() {
     if (activeIndex == urlImages.length - 1) {
--- lib/app/modules/home/views/first_start_view.dart
+++ lib/app/modules/home/views/first_start_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_healthcare/app/modules/home/controllers/first_start_controller.dart';
 import 'package:get/get.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class FirstStartView extends StatefulWidget {
   const FirstStartView({Key? key}) : super(key: key);
@@ -14,6 +15,14 @@ class FirstStartView extends StatefulWidget {
 class _FirstStartViewState extends State<FirstStartView> {
   final FirstStartController firstStartController =
       Get.put(FirstStartController());
+  var buttonStyle = ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      foregroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent),
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              side: BorderSide(color: Colors.blueAccent))));
+  String textButton = 'SKIP';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,24 +51,72 @@ class _FirstStartViewState extends State<FirstStartView> {
               final textBelowImage =
                   firstStartController.textBelowImages[index];
               final note = firstStartController.notes[index];
-              return firstStartController.buildImage(
-                  urlImage, textBelowImage, note, index);
+              return buildImage(urlImage, textBelowImage, note, index);
             },
           ),
         ]),
-        firstStartController.buildIndicator(),
-        Obx(() => ConstrainedBox(
-              constraints: BoxConstraints(minWidth: 100, minHeight: 50),
-              child: TextButton(
-                onPressed: firstStartController.next,
-                child: Text(
-                  '${firstStartController.textButton}',
-                  style: TextStyle(fontSize: 24),
-                ),
-                style: firstStartController.buttonStyle.value,
-              ),
-            ))
+        buildIndicator(),
+        ConstrainedBox(
+          constraints: BoxConstraints(minWidth: 100, minHeight: 50),
+          child: TextButton(
+            onPressed: firstStartController.next,
+            child: Text(
+              textButton,
+              style: TextStyle(fontSize: 24),
+            ),
+            style: buttonStyle,
+          ),
+        )
       ],
     )));
+  }
+
+  Widget buildImage(
+          String urlImage, String textBelowImage, String note, int index) =>
+      Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fitWidth,
+                image: AssetImage(urlImage),
+              ),
+            ),
+          ),
+        ),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(textBelowImage, style: TextStyle(fontSize: 24)),
+          Text(note)
+        ])
+      ]);
+
+  Widget buildIndicator() {
+    setState(() {
+      if (firstStartController.activeIndex ==
+          firstStartController.urlImages.length - 1) {
+        textButton = 'NEXT';
+        buttonStyle = ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.blueAccent),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    side: BorderSide(color: Colors.blueAccent))));
+      } else {
+        textButton = 'SKIP';
+        buttonStyle = ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            foregroundColor:
+                MaterialStateProperty.all<Color>(Colors.blueAccent),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    side: BorderSide(color: Colors.blueAccent))));
+      }
+    });
+    return AnimatedSmoothIndicator(
+        activeIndex: firstStartController.activeIndex,
+        count: firstStartController.urlImages.length);
   }
 }
