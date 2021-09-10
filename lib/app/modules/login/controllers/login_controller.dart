962c8cd02c6127f7199f39b732f44cfe87a8b952
Truong Bien <thanhtruonginfo@gmail.com>
2021-09-11 02:16:54 +0700

    Login UI

---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+}
