d8c8c99b65dc1460b1e518daed28520a215e569f
Vo Tu Thien <vothien220518@gmail.com>
2021-09-14 11:19:39 +0700

    Chuyen sang extend GitView<>

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,11 +1,14 @@
 import 'package:carousel_slider/carousel_controller.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_healthcare/app/modules/home/views/home_view.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
-  final controller = CarouselController();
-  int activeIndex = 0;
+  final carouselController = CarouselController();
+  RxInt activeIndex = 0.obs;
   String textButton = 'SKIP';
+  var bgColor = MaterialStateProperty.all<Color>(Colors.white).obs;
+  var fgColor = MaterialStateProperty.all<Color>(Colors.blueAccent).obs;
   final urlImages = [
     'assets/images/first_start_image_1.png',
     'assets/images/first_start_image_2.png',
@@ -23,9 +26,21 @@ class SplashController extends GetxController {
   ];
 
   next() {
-    if (activeIndex == urlImages.length - 1) {
+    if (activeIndex.value == urlImages.length - 1) {
       Get.to(HomeView());
     } else
-      controller.nextPage();
+      carouselController.nextPage();
+  }
+
+  changeButton(int index) {
+    if (activeIndex.value == urlImages.length - 1) {
+      bgColor = MaterialStateProperty.all<Color>(Colors.blueAccent).obs;
+      fgColor = MaterialStateProperty.all<Color>(Colors.white).obs;
+      textButton = 'NEXT';
+    } else {
+      bgColor = MaterialStateProperty.all<Color>(Colors.white).obs;
+      fgColor = MaterialStateProperty.all<Color>(Colors.blueAccent).obs;
+      textButton = 'SKIP';
+    }
   }
 }
